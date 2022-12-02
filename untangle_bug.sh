41920d26cdb5b6ac73c07f9e63ea33e32667a1a1
Thomas Schweizer <tschweiz@cs.washington.edu>
2022-12-02 13:31:07 -0800

    Move ground truth for Defect4J out of main script

--- untangle_bug.sh
+++ untangle_bug.sh
@@ -13,13 +13,7 @@ work_dir="./tmp"
 revision_id="687b2e62"
 defects4j_framework="/Users/thomas/Workplace/defects4j/framework"
 
-# Ground truth:
-project="Cli"
-vid="1"
-# Show patch
-# cat $defects4j_framework/projects/$project/patches/$vid.src.patch
-cat /Users/thomas/Workplace/defects4j/framework/projects/Lang/patches/1.src.patch | ./diff-lines.sh
-# Diff-lines is probably not getting the right line numbers (new rather than old file (vn to vbug)).
+
 
 # Get changes from decomposition.
 git clone "/Users/thomas/Workplace/defects4j/project_repos/commons-lang.git" "$work_dir" 2>&1 && cd "$work_dir" && git checkout $revision_id 2>&1 # Probably better to use defects4j checkout to avoid broken stuff. Checkout the tag.
