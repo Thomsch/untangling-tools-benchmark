94bd6ea66a9dd990a315d8952b969d8b6cbb5b9b
Thomas Schweizer <tschweiz@cs.washington.edu>
2022-12-20 14:44:05 -0800

    Refactor to parse patch into csv from file or stdin

--- parse_patch.py
+++ parse_patch.py
@@ -5,16 +5,29 @@ import sys
 from unidiff import PatchSet
 from unidiff.constants import LINE_TYPE_CONTEXT
 
-def main():
-    patch = PatchSet.from_string(sys.stdin.read())
-
+def to_csv(patch: PatchSet):
     for file in patch:
         for hunk in file:
             for line in hunk:
                 if line.line_type == LINE_TYPE_CONTEXT:
                     continue;
-                # print(line.target_line_no, file.path, line.value.strip())
-                print(f'{file.path}, {line.source_line_no}, {line.target_line_no}, \"{line.value.strip()}\"')
+                yield f'{file.path},{line.source_line_no},{line.target_line_no}' #,\"{line.value.strip()}\"'
+
+
+def from_file(filename):
+    patch = PatchSet.from_filename(filename)
+
+    result = ''
+    for line in to_csv(patch):
+        result += line + '\n'
+
+    return result
+
+def main():
+    patch = PatchSet.from_string(sys.stdin.read())
+
+    for line in to_csv(patch):
+        print(line)
 
 if __name__ == "__main__":
     main()
