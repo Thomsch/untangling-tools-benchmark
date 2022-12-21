895762bfb5938f6d553241fdd9b987d47781e723
Thomas Schweizer <tschweiz@cs.washington.edu>
2022-12-20 21:27:18 -0800

    Update result format for groups

--- retrieve_changed_lines.py
+++ retrieve_changed_lines.py
@@ -3,6 +3,12 @@ import sys
 import json
 import glob
 
+from io import StringIO
+from unidiff import PatchSet
+
+import ground_truth
+import pandas as pd
+
 # Retrieves changed lines for SmartCommit results.
 
 # Exports line changed from smartcommit group
@@ -25,11 +31,13 @@ def list_json_files(dir):
 def main():
     args = sys.argv[1:]
 
-    if len(args) != 1:
-        print("usage: file.py <path/to/root/results>")
+    if len(args) != 2:
+        print("usage: this_script.py <path/to/root/results> <path/to/out/file>")
         exit(1)
     
     result_dir = args[0]
+    output_path = args[1]
+
     diff_dir = os.path.join(result_dir, 'diffs')
     groups_dir = os.path.join(result_dir, 'generated_groups')
 
@@ -45,14 +53,18 @@ def main():
             hunks = {}
             diff_data[file_id] = hunks
 
+            hunks['rawHeaders'] = data['rawHeaders']
+
             # Save data for each hunk
             for hunk_data in data['diffHunksMap'].values():
                 hunk_id = hunk_data['diffHunkID']
                 startLine = hunk_data['currentHunk']['startLine']
                 endLine = hunk_data['currentHunk']['endLine']
                 
-                hunks[hunk_id] = (class_path, startLine, endLine)
+                hunks[hunk_id] = (class_path, startLine, endLine, hunk_data['rawDiffs'])
                 
+
+    result = ''
 
     # Print CSV for each group
     for group_path in list_json_files(groups_dir):
@@ -60,13 +72,27 @@ def main():
             data = json.load(group_file)
 
             hunks = data['diffHunkIDs']
-
+            group_id = data['groupID']
+            
             for hunk in hunks:
                 file_id, hunk_id = hunk.split(':')
-                class_path, start_line, end_line = diff_data[file_id][hunk_id]
+                class_path, start_line, end_line, rawDiff = diff_data[file_id][hunk_id]
 
-                for line in range(start_line, end_line + 1):
-                    print(f"{group_path},{class_path},{line}")
+                header_str = '\n'.join(diff_data[file_id]['rawHeaders'])
+                diff_str = '\n'.join(rawDiff)
+
+                import parse_patch
+                patch = PatchSet.from_string(header_str + '\n' + diff_str)
+                for line in parse_patch.to_csv(patch):
+                    result += f'{group_id}, {line}\n'
+
+    # Export results
+    df = pd.read_csv(StringIO(result), names=['group', 'file', 'source', 'target'], na_values='None')
+    df = df.convert_dtypes() # Forces pandas to use ints in source and target columns.
+    df.to_csv(output_path, index=False)
+
 
 if __name__ == "__main__":
     main()
+
+# LocalWords: unidiff
