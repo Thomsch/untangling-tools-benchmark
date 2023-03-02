6998650b88234b8b96f6829a16ea08a2810a1625
Thomas Schweizer <tschweiz@cs.washington.edu>
2023-03-02 13:44:02 -0800

    Various fixes
    Use log dir instead of results directory
    Update metrics to use output root
    Update score aggregation with latest root

--- evaluate_all.sh
+++ evaluate_all.sh
@@ -11,9 +11,14 @@ out_dir=$2 # Path to the directory where the results are stored and repositories
 results="${out_dir}/results" # Contains the results for each commit.
 out_file="${out_dir}/decompositions.csv" # Aggregated results.
 workdir="${out_dir}/repositories"
+metrics_dir="${out_dir}/metrics"
+evaluation_dir="${out_dir}/evaluation"
+logs_dir="${out_dir}/logs"
 
 mkdir -p "$results"
 mkdir -p "$workdir"
+mkdir -p "$metrics_dir"
+mkdir -p "$logs_dir"
 
 if ! [[ -f "$bugs_file" ]]; then
     echo "File ${bugs_file} not found. Exiting."
@@ -21,7 +26,7 @@ if ! [[ -f "$bugs_file" ]]; then
 fi
 
 # TODO: Parallelize
-echo "Logs stored in ${results}/<project>_<bud_id>.log"
+echo "Logs stored in ${logs_dir}/<project>_<bud_id>.log"
 echo ""
 
 error_counter=0
@@ -29,7 +34,7 @@ while IFS=, read -r project vid
 do
     # TODO: Don't regenerate results when they already exist.
     START=$(date +%s.%N)
-    ./evaluate.sh "$project" "$vid" "$out_dir" "$workdir" &> "${results}/${project}_${vid}.log"
+    ./evaluate.sh "$project" "$vid" "$out_dir" "$workdir" &> "${logs_dir}/${project}_${vid}.log"
     ret_code=$?
     evaluation_status=$([ $ret_code -ne 0 ] && echo "FAIL" || echo "OK")
     END=$(date +%s.%N)
@@ -38,22 +43,21 @@ do
         error_counter=$((error_counter+1))
     fi
     printf "%-20s %s (%.0fs)\n" "${project}_${vid}" "${evaluation_status}" "${DIFF}"
-    
+
 done < "$bugs_file"
 
 echo ""
 echo "Evaluation finished with ${error_counter} errors out of $(wc -l < $bugs_file) commits."
 
-cat "${results}"/*.csv > "$out_file"
+cat "${evaluation_dir}"/*/*/scores.csv > "$out_file"
 #find ${results} -name "*.csv" -type f -delete
 
 echo ""
 echo "Decomposition scores aggregated and saved in ${out_file}"
 
-metrics_dir="out/metrics"
-metrics_results="out/metrics.csv"
+metrics_results="${out_dir}/metrics.csv"
 
-cat ${metrics_dir}/*.csv > $metrics_results
+cat "${metrics_dir}"/*.csv > "$metrics_results"
 # find ${metrics_dir} -name "*.csv" -type f -delete
 
 echo ""
