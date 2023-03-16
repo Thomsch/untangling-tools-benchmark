6a8c9404c798985520f829116f31e77aca49b5f1
Thomas Schweizer <tschweiz@cs.washington.edu>
2023-03-16 15:49:42 -0700

    Clean up file

--- scripts/ground_truth.sh
+++ scripts/ground_truth.sh
@@ -2,16 +2,6 @@
 #
 # Retrieves the minimal bug-fixing changes for a Defect4J bug.
 #
-# Saves the results in a table:
-# class     | line changed
-# Foo.java  | 3
-# Foo.java  | 5
-# Bar.java  | 230
-# Bar.java  | 231
-# Bar.java  | 232
-#
-
-# Ground truth:
 project=$1
 vid=$2
 repository=$3
@@ -20,4 +10,3 @@ commit=$5
 
 export DEFECTS4J_HOME="/Users/thomas/Workplace/defects4j"
 ./scripts/changed_lines.sh "$project" "$vid" "$repository" "$commit" | python3 src/ground_truth.py "$project" "$vid" "$truth_out"
-# Diff-lines is probably not getting the right line numbers (new rather than old file (vn to vbug)).
