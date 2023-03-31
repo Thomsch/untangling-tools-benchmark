00e10074e1d4cbc0fb7d346fb334b1750d487223
Thomas Schweizer <tschweiz@cs.washington.edu>
2023-03-31 14:22:21 -0700

    Statistical significance and effect size with random effects

---
+++ analysis/RQ1.R
@@ -0,0 +1,34 @@
+library(tidyverse)
+library(car)
+library(rstatix)
+library(ggpubr)
+library(lme4)
+library(ggbeeswarm)
+library(effsize)
+
+#
+# We use R to handle the linear mixed models because Python doesn't support lmms with 2 random effects (cross effects).
+#
+
+
+fileData <-"./decompositions-100.csv"
+data <- read.csv(fileData, header = FALSE, col.names = c('Project', 'BugID', 'SmartCommit', 'Flexeme'))
+
+# Convert to long format
+data_long = pivot_longer(data, cols = 3:4, names_to = 'Treatment', values_to = 'Performance')
+
+ggplot(data_long, aes(x=Treatment, y=Performance)) + geom_beeswarm() + coord_flip()
+
+model <- lmer(Performance ~ Treatment + (1|Project) + (1|BugID), data=data_long)
+summary(model)
+# If Estimate for SmartCommit or Flexeme is smaller than 0.05, then there is a significant difference.
+
+# Effect size
+cohen.d(data_long$Performance[data_long$Treatment == "SmartCommit"], data_long$Performance[data_long$Treatment == "Flexeme"])
+
+#fit <- lme(Performance ~ Treatment, random=c(~1|Project, ~1|BugID), data=data)
+#summary(fit)
+
+# Use random effects on Project and bug id because they are measured multiple times.
+# model <- lmer(SmartCommitRand ~ FlexemeRand + (1|Project) + (1|BugID), data=data)
+# summary(model)
---
+++ analysis/RQ2.R
@@ -0,0 +1,28 @@
+library(tidyverse)
+library(car)
+library(rstatix)
+library(ggpubr)
+library(lme4)
+library(ggbeeswarm)
+library(effsize)
+
+#
+# We load directly in the long format because I already written the code in Python for it :).
+# To generate the file, run the jupyter notebook in this folder.
+#
+
+library(lmerTest)
+
+fileData <-"./merge-100.csv"
+data <- read.csv(fileData)
+data <- na.omit(data)
+
+
+# Convert to long format
+data_long = pivot_longer(data, cols = c(smartcommit_rand_index, flexeme_rand_index), names_to = 'Treatment', values_to = 'Performance')
+
+model <- lmer(Performance ~ Treatment + files_updated + test_files_updated + hunks + average_hunk_size + lines_updated + (1|project) + (1|bug_id), data=data_long)
+summary(model)
+
+cohen.d(data_long$average_hunk_size, data_long$Performance)
+cohen.d(data_long$lines_updated, data_long$Performance)
