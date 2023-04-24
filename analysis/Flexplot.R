2634db24a3a0945139622190a0140a22980a5301
Thomas Schweizer <tschweiz@cs.washington.edu>
2023-04-24 13:22:14 -0700

    Flexplot tests

---
+++ analysis/Flexplot.R
@@ -0,0 +1,15 @@
+install.packages("devtools")
+devtools::install_github("dustinfife/flexplot")
+
+library(flexplot)
+data(relationship_satisfaction)
+
+### multivariate relationship
+flexplot(satisfaction~communication + separated | gender + interests, data=relationship_satisfaction)
+
+flexplot(gender~1, data=relationship_satisfaction)
+
+fileData <-"./lines.csv"
+data <- read.csv(fileData, header = FALSE, col.names = c('project','bug_id','fix_lines','other_lines'))
+
+flexplot(project~1, data=data)
