b2e1a77f43e141b85e9394c7e19938d2520e0f82
Thomas Schweizer <tschweiz@cs.washington.edu>
2023-05-04 14:55:38 -0700

    Implement descriptive statistics

---
+++ analysis/DescriptiveStatistics.R
@@ -0,0 +1,42 @@
+library(tidyverse)
+library(car)
+library(rstatix)
+library(ggpubr)
+library(lme4)
+library(ggbeeswarm)
+library(effsize)
+library(lmerTest)
+library(flexplot)
+library(ggplot2)
+
+
+fileData <-"./collated_decompositions.csv"
+data <- read.csv(fileData, header = FALSE, col.names = c('Project', 'BugID', 'Treatment', 'File', 'Source', 'Target', 'Group'))
+data$BugID <- as_factor(data$BugID)
+
+#
+# Number of groups in a decomposition
+#
+groupCount <- data %>% group_by(Project, BugID, Treatment) %>% summarise(GroupCount = n_distinct(Group))
+groupCount
+
+# Summary of group count
+groupCount %>% group_by(Treatment) %>% summarise(Mean=mean(GroupCount), Max=max(GroupCount), Min=min(GroupCount), Median=median(GroupCount), Std=sd(GroupCount))
+
+# Distribution of group count
+plt <- flexplot(GroupCount ~ Treatment, data=groupCount, spread = "quartile")
+plt$labels$y = "Number of Groups"
+plt
+
+#
+# Size of groups in a decomposition
+#
+groupSize <- data %>% group_by(Project, BugID, Treatment, Group) %>% summarise(n = n())
+groupSize
+
+groupSize %>% group_by(Treatment) %>% summarise(Mean=mean(n), Max=max(n), Min=min(n), Median=median(n), Std=sd(n))
+
+
+plt <- flexplot(n ~ Treatment, data=groupSize, spread = "quartile")
+plt$labels$y = "Group Size"
+plt
