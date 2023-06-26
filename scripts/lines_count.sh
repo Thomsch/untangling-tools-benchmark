254e671362a5ae59fe67ba48307339c6e80f8998
Thomas Schweizer <tschweiz@cs.washington.edu>
2023-06-26 16:37:08 -0700

    Explain why lines_count.sh continues when a ground_truth cannot be calculated

--- scripts/lines_count.sh
+++ scripts/lines_count.sh
@@ -3,9 +3,18 @@
 # The script calls ground_truth.sh to generate a truth.csv file, then calls count_lines.py to count 'fix' versus 'other' commits.
 # - $1: Path where the D4J bug file is stored.
 # - $2: Path where the line counting result is checked out
+#
 # Writes a lines.csv file (with 1 row) to the specified path.
 # - CSV header: project,bug_id,fix_lines=number of bug-fixing lines,nonfix_line=number of non bug-fixing lines
-
+#
+# If the ground truth cannot be calculated for a commit, the script will tag the commit as 'FAIL' in the output file.
+# When the ground truth cannot be calculated for a commit, the script will tag the commit as 'FAIL' in the output file
+# and proceed to the next commit. This avoid having to restart the script everytime there is an issue with a commit and
+# allows users to fix problematic commits while the rest of the ground truth is being calculated.
+#
+# When the problematic commits are fixed, the script can be re-run to calculate the ground truth for the commits that
+# failed. The script will not re-calculate the ground truth for a commit if the ground truth has already been calculated.
+#
 set -o errexit    # Exit immediately if a command exits with a non-zero status
 set -o nounset    # Exit if script tries to use an uninitialized variable
 set -o pipefail   # Produce a failure status if any command in the pipeline fails
@@ -53,7 +62,6 @@ do
         # TODO: Use an if statement to avoid spawning a new subshell.
         evaluation_status_string=$([ $ret_code -ne 0 ] && echo "FAIL" || echo "OK")
         echo -ne "Calculating ground truth .................................................. ${evaluation_status_string}\n"
-        ## TODO: Why does this proceed if there is a failure?  Would it be better to stop, so that the experimenter has to fix it?
     fi
 
     # count number of lines and append
