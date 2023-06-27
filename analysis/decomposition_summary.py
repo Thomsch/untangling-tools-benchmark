67b271024420ee347c4403ff10ad418906cb5f7a
Thomas Schweizer <tschweiz@cs.washington.edu>
2023-06-27 14:05:52 -0700

    Improve documentation for decomposition_summary.py

--- analysis/decomposition_summary.py
+++ analysis/decomposition_summary.py
@@ -1,19 +1,21 @@
 #!/usr/bin/env python3
+
+"""
+This scripts prints whether a tool decomposition is present for each bug in the benchmark.
+A tool decomposition is considered present if the tool did not crash and produced a result file (e.g., flexeme.csv).
+
+Input: the path to the evaluation foot folder.
+
+Output: a row for each bug in the benchmark, with the following columns:
+   - project: the name of the project
+   - bug_id: the id of the bug
+   - flexeme_exists: true if flexeme decomposition is present, false otherwise
+   - smartcommit_exists: true if smartcommit decomposition is present, false otherwise
+"""
 import os
 import sys
 
 
-#
-# This script lists missing tool decompositions in the benchmark for each bug.
-## TODO: I think this script lists *all* tool decompositions, not just missing ones.
-## TODO: Define "missing tool decompositions".  Does that mean an untangling tool crashed?
-# Input: the path to the evaluation foot folder.
-# Output: a row for each bug in the benchmark, with the following columns:
-#   - project: the name of the project
-#   - bug_id: the id of the bug
-#   - flexeme_exists: true if flexeme decomposition is present, false otherwise
-#   - smartcommit_exists: true if smartcommit decomposition is present, false otherwise
-#
 def main(path):
     print("project,bug_id,flexeme_exists,smartcommit_exists")
 
