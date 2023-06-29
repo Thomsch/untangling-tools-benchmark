f02b44dac462fc196d140037d641a5cde1df348c
thanhdang2712 <danghuyenthanh2003@gmail.com>
2023-06-28 17:02:48 -0700

    Rename JAVA11_HOME variable

--- test/check_installation.sh
+++ test/check_installation.sh
@@ -28,8 +28,8 @@ if [[ $(echo "$java_version != 1.8" | bc) == 1 ]] ; then
 fi
 
 # Check JAVA 11 is installed and on PATH. Defects4J will use whatever is on JAVA_HOME.
-if [[ -z "${JAVA_11}" ]]; then
-  echo 'JAVA_11 environment variable is not set.'
+if [[ -z "${JAVA11_HOME}" ]]; then
+  echo 'JAVA11_HOME environment variable is not set.'
   echo 'Please set it to the path of a Java 11 java.'
   exit 1
 fi
