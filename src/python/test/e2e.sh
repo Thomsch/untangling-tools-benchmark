0ff655f288a795968997b0cbd620f0dd42501bdc
thanhdang2712 <danghuyenthanh2003@gmail.com>
2023-07-06 17:04:15 -0700

    New end to end goal files due to bug in ground truth

--- src/python/test/e2e.sh
+++ src/python/test/e2e.sh
@@ -9,14 +9,14 @@ set -o nounset
 set -o pipefail
 
 if [[ $# -ne 0 ]] ; then
-    echo 'usage: ./test/e2e.sh'
+    echo 'usage: ./src/python/test/e2e.sh'
     exit 1
 fi
 
 export PYTHONHASHSEED=0         # Make test deterministic
 
 bugs_file="data/d4j-5-bugs.csv" # Path to the file containing the bugs to untangle and evaluate.
-out_dir="test/e2e" # Path to the directory where the results are stored and repositories checked out.
+out_dir="src/python/test/e2e" # Path to the directory where the results are stored and repositories checked out.
 
 metrics_goal="${out_dir}/metrics_goal.csv"
 decompositions_goal="${out_dir}/decompositions_goal.csv"
