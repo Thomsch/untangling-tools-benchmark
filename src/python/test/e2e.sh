f537b339599f9d1d0c5648ef848e66ade1776893
Michael Ernst <mernst@cs.washington.edu>
2023-07-11 20:23:39 -0700

    Renaming

--- src/python/test/e2e.sh
+++ src/python/test/e2e.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-# Run the evaluation pipeline end-to-end on 5 bug files: Closure_78, Closure_98, CSV_8, Jsoup_24, Lang_63.
+# Run the evaluation pipeline end-to-end on the bugs in file "data/d4j-5-bugs.csv".
 # The decomposition and untangling results are stored in the /e2e folder. 
-# The script calls evaluate.sh on the 5 bugs and writes to 2 output files: `decompositions.csv` and `metrics.csv`.
+# The script calls evaluate.sh on the 5 bugs and writes to 2 output files: `rand_index_scores.csv` and `metrics.csv`.
 # These 2 aggregated files are expected to be identical to the corresponding goal files.
 
 set -o errexit
@@ -19,13 +19,13 @@ bugs_file="data/d4j-5-bugs.csv" # Path to the file containing the bugs to untang
 out_dir="src/python/test/e2e" # Path to the directory where the results are stored and repositories checked out.
 
 metrics_goal="${out_dir}/metrics_goal.csv"
-decompositions_goal="${out_dir}/decomposition_scores_goal.csv"
+rand_index_scores_goal="${out_dir}/rand_index_scores_goal.csv"
 
 # Run the 5_bug example and write output files to /e2e
 ./evaluate_all.sh "$bugs_file" "$out_dir"
 
 metrics_results="${out_dir}/metrics.csv"
-decompositions_results="${out_dir}/decomposition_scores.csv"
+rand_index_scores_results="${out_dir}/rand_index_scores.csv"
 
 # Diff the aggregated metrics file with the goal file
 if ! diff -u "$metrics_goal" "$metrics_results"; then
@@ -34,7 +34,7 @@ if ! diff -u "$metrics_goal" "$metrics_results"; then
 fi
 
 # Diff the aggregated Rand Index scores file with the goal file
-if ! diff -u "$decompositions_goal" "$decompositions_results"; then
+if ! diff -u "$rand_index_scores_goal" "$rand_index_scores_results"; then
     echo "Error: The Rand Index scores computed are different."
     exit 1
 fi
