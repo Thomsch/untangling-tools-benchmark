428211e17b110c8f40aef4b967299f51e7a7e115
thanhdang2712 <danghuyenthanh2003@gmail.com>
2023-07-14 13:20:48 -0700

    Fixed bug: unable to find absolute paths

--- src/python/test/e2e.sh
+++ src/python/test/e2e.sh
@@ -15,11 +15,12 @@ fi
 
 export PYTHONHASHSEED=0         # Make Flexeme deterministic
 
-bugs_file="data/d4j-5-bugs.csv" # Path to the file containing the bugs to untangle and evaluate.
-out_dir="src/python/test/e2e" # Path to the directory where the results are stored and repositories checked out.
+export workdir="$(pwd)"
+export bugs_file="${workdir}/data/d4j-5-bugs.csv" # Path to the file containing the bugs to untangle and evaluate.
+export out_dir="${workdir}/src/python/test/e2e" # Path to the directory where the results are stored and repositories checked out.
 
-metrics_goal="${out_dir}/metrics_goal.csv"
-decomposition_scores_goal="${out_dir}/decomposition_scores_goal.csv"
+export metrics_goal="${out_dir}/metrics_goal.csv"
+export decomposition_scores_goal="${out_dir}/decomposition_scores_goal.csv"
 
 
 # Run the 5_bug example and write output files to /e2e
