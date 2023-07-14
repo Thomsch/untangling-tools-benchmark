74cd2ddb54ff839b2f1f000c927551e9a72d707d
thanhdang2712 <danghuyenthanh2003@gmail.com>
2023-07-14 10:52:10 -0700

    Reformat with Black

--- src/python/main/diff_metrics.py
+++ src/python/main/diff_metrics.py
@@ -197,7 +197,9 @@ def main():
     clean_diff(
         path.join(repository, "diff", "VC.diff")
     )  # Remove blank lines, comments, import statements from VC diff for tangled line and hunk support
-    print("project,vid,files_updated,test_files_updated,hunks,average_hunk_size,lines_updated,tangled_lines_count,tangled_hunks_count")
+    print(
+        "project,vid,files_updated,test_files_updated,hunks,average_hunk_size,lines_updated,tangled_lines_count,tangled_hunks_count"
+    )
     print(
         f"{project},{vid},{files_updated},{test_files_updated},"
         f"{hunks},{average_hunk_size},{lines_updated},"
