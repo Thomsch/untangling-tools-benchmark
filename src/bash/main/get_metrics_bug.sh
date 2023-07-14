8cc8a08bc43d242a80562407c5ac0b4a41c19072
thanhdang2712 <danghuyenthanh2003@gmail.com>
2023-07-14 10:19:11 -0700

    Use new script to checkout D4J repo instead of old command

--- src/bash/main/get_metrics_bug.sh
+++ src/bash/main/get_metrics_bug.sh
@@ -37,8 +37,10 @@ echo -ne '\n'
 echo "Calculating ground truth for project $project, bug $vid, repository $repository"
 
 # Checkout Defects4J bug
-mkdir -p "$repository"
-defects4j checkout -p "$project" -v "$vid"b -w "$repository"
+if [ ! -d "${repository}" ] ; then
+  mkdir -p "$repository"
+  ./src/bash/main/generate_artifacts_bug.sh "$project" "$vid" "$repository"
+fi
 
 metrics_csv="${metrics_dir}/${project}_${vid}.csv" # Metrics for this bug
 
