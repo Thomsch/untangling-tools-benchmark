1652b13701caad2921279387c205df770683443f
Michael Ernst <mernst@cs.washington.edu>
2023-07-18 20:05:27 -0700

    Add missing space

--- src/bash/main/decompose_bug.sh
+++ src/bash/main/decompose_bug.sh
@@ -123,7 +123,7 @@ echo -ne '\n'
 echo -ne 'Parsing Flexeme results ...............................................\r'
 
 flexeme_result_out="${evaluation_path}/flexeme.csv"
-if [ "${flexeme_untangling_code:-1}" -ne 0 ] || { [ -f "$flexeme_result_out" ] && [ $regenerate_results =false ]; } ;
+if [ "${flexeme_untangling_code:-1}" -ne 0 ] || { [ -f "$flexeme_result_out" ] && [ $regenerate_results = false ]; } ;
 then
   echo -ne 'Parsing Flexeme results ................................................. CACHED\r'
 else
