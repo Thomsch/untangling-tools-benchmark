41f0fc7de7c375cbe2371b00e9844838a5b61d72
Thomas Schweizer <tschweiz@cs.washington.edu>
2023-07-28 15:44:55 -0700

    Replace ambiguous name

--- analysis/paper/count_missing_results.py
+++ analysis/paper/count_missing_results.py
@@ -9,11 +9,11 @@ This script counts the missing result files in the evaluation. i.e.:
     - file_untangling.csv
 
 This script prints the following on the standard output:
-- Total number of projects visited
-- Total number of projects with missing files (i.e., projects that are missing at least one of the required files)
+- Total number of bug result folders existing in the evaluation directory.
+- Total number of bug result folders with missing files (i.e., bug results that are missing at least one result file)
 - Number of times each result file is missing.
-- The following projects are missing the following files:
-    - <project_name>: <missing_file_1>, <missing_file_2>, ..
+- List of bug result folders and the missing files in each folder. i.e.:
+    - <bug folder>: <missing_file_1>, <missing_file_2>, ..
 """
 
 import os
@@ -33,8 +33,8 @@ def main(evaluation_dir):
         "file_untangling.csv"
     }
     missing_files = []
-    projects_missing_files = defaultdict(int)
-    project_counter = 0
+    bug_folder_missing_files = defaultdict(int)
+    bug_folder_counter = 0
 
     # Walk through the directory tree starting from 'evaluation'
     for root, dirs, files in os.walk(evaluation_dir):
@@ -43,18 +43,18 @@ def main(evaluation_dir):
         if root != "evaluation" and len(files) > 0:
             # Check if all the required CSV files are present in the bug_name folder
             missing_files_for_project = required_files - set(files)
-            project_counter += 1
+            bug_folder_counter += 1
             if missing_files_for_project:
                 missing_files.append(
                     (bug_name, list(missing_files_for_project))
                 )
-                projects_missing_files[bug_name] += len(
+                bug_folder_missing_files[bug_name] += len(
                     missing_files_for_project
                 )
 
     # Print the list of projects and missing files
-    print(f"Total number of projects visited: {project_counter}")
-    print(f"Total number of projects with missing files: {len(missing_files)}")
+    print(f"Total number of bug folder visited: {bug_folder_counter}")
+    print(f"Total number of bug folder with missing files: {len(missing_files)}")
     print("Number of times each file is missing:")
     for required_file in required_files:
         count = sum(
@@ -63,7 +63,7 @@ def main(evaluation_dir):
             if required_file in project_missing[1]
         )
         print(f"{required_file}: {count}")
-    print("The following projects are missing the following files:")
+    print("The following bug folders are missing the following files:")
     for bug_name, missing in missing_files:
         print(f'{bug_name}: {", ".join(missing)}')
 
