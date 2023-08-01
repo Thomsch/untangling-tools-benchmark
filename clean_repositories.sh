6729e88a99bfe448a976c7b7091f09cdd5818e6c
Thomas Schweizer <tschweiz@cs.washington.edu>
2023-07-31 22:25:03 -0700

    Clean ids and such

--- clean_repositories.sh
+++ clean_repositories.sh
@@ -26,8 +26,50 @@ clean_repo() {
     rm -rf "$repo_dir/.git"
     rm -rf "$repo_dir/.github"
     rm -rf "$repo_dir/.vscode"
-    
+
 }
 
 clean_repo "$out_dir/untangling-tools-benchmark"
 clean_repo "$out_dir/flexeme"
+
+# Replace Github user ids thomsch, thanhdang2712, rjust, mernst with 'anonymous'
+find "$out_dir" -type f -print0 | xargs -0 perl -pi -e 's/thomsch/anonymous/gi'
+find "$out_dir" -type f -print0 | xargs -0 perl -pi -e 's/thanhdang2712/anonymous/gi'
+find "$out_dir" -type f -print0 | xargs -0 perl -pi -e 's/rjust/anonymous/gi'
+find "$out_dir" -type f -print0 | xargs -0 perl -pi -e 's/mernst/anonymous/gi'
+
+find "$out_dir" -type f -print0 | xargs -0 perl -pi -e 's%anonymous/defects4j%rjust/defects4j%gi' # if we anonymize legitimate link to defects4j, it is suspicious.
+
+# Replace first and or last names with 'Anonymous'
+# Names: Thomas Schweizer, Thanh Dang, Rene Just, Mike Ernst
+# Refactor the code to use a function that takes a full name as a parameter
+
+replace_name() {
+    full_name="$1"
+    first_name=$(echo "$full_name" | cut -d' ' -f1)
+    last_name=$(echo "$full_name" | cut -d' ' -f2)
+
+    find "$out_dir" -type f -print0 | xargs -0 perl -pi -e "s/$first_name/Anonymous/gi"
+    find "$out_dir" -type f -print0 | xargs -0 perl -pi -e "s/$last_name/Anonymous/gi"
+    find "$out_dir" -type f -print0 | xargs -0 perl -pi -e "s/$full_name/Anonymous/gi"
+}
+
+replace_name "Thomas Schweizer"
+replace_name "Thanh Dang"
+replace_name "Rene Just"
+replace_name "Michael Ernst"
+replace_name "Mike Ernst"
+
+
+# Replace school ids tschweiz, thanh, rjust, mernst with 'anonymous'
+find "$out_dir" -type f -print0 | xargs -0 perl -pi -e 's/tschweiz/anonymous/gi'
+find "$out_dir" -type f -print0 | xargs -0 perl -pi -e 's/thanh/anonymous/gi'
+find "$out_dir" -type f -print0 | xargs -0 perl -pi -e 's/rjust/anonymous/gi'
+find "$out_dir" -type f -print0 | xargs -0 perl -pi -e 's/mernst/anonymous/gi'
+
+find "$out_dir" -type f -print0 | xargs -0 perl -pi -e 's%anonymous/defects4j%rjust/defects4j%gi' # if we anonymize legitimate link to defects4j, it is suspicious.
+
+# Replace school name with 'anonymous'
+find "$out_dir" -type f -print0 | xargs -0 perl -pi -e 's/washington/anonymous/gi'
+find "$out_dir" -type f -print0 | xargs -0 perl -pi -e 's/cs.washington/anonymous/gi'
+# The is no occurence of the school acronym anywhere
