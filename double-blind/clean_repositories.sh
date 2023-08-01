5f9a663a93c10e478b5b8c87a9b8d67ca7fba3b9
Thomas Schweizer <tschweiz@cs.washington.edu>
2023-08-01 13:03:44 -0700

    Improve script

--- double-blind/clean_repositories.sh
+++ double-blind/clean_repositories.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 # Cleans this repository and Flexeme's for the double-blind paper submission.
+# Before running this script, create "$out_dir", and move untangling result and the analysis results into it.
 
 set -o errexit    # Exit immediately if a command exits with a non-zero status
 set -o nounset    # Exit if script tries to use an uninitialized variable
@@ -10,15 +11,15 @@ if [ $# -ne 1 ] ; then
     exit 1
 fi
 
-
-export out_dir="$1" # The directory where to put cleaned repositories
-
+export out_dir="$1" # The directory where to put cleaned repositories (e.g., 'artifacts')
 
 mkdir -p "$out_dir"
 
 # Clone repositories
 git clone --depth 1 https://github.com/Thomsch/untangling-tools-benchmark "$out_dir/untangling-tools-benchmark"
 git clone --depth 1 https://github.com/Thomsch/flexeme "$out_dir/flexeme"
+echo "Cloned repositories into $out_dir"
+echo "Cleaning all files in $out_dir"
 
 # Delete .git .github .vscode folders in both repositories
 clean_repo() {
@@ -78,3 +79,5 @@ find "$out_dir" -type f -print0 | xargs -0 perl -pi -e 's/cs.washington/anonymou
 # Move double-blind/README.MD to $OUT_DIR
 cp double-blind/README.MD "$out_dir"
 
+# Remove cleaning script and README.MD from the cleaned repository
+rm -rf "$out_dir/untangling-tools-benchmark/double-blind"
