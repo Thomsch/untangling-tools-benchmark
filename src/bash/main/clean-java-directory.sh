4dfad87e1810491e1224bc792b3bf79b104eec66
Michael Ernst <mernst@cs.washington.edu>
2023-09-01 16:18:07 -0700

    Shell settings to discover run-time failures

--- src/bash/main/clean-java-directory.sh
+++ src/bash/main/clean-java-directory.sh
@@ -6,7 +6,9 @@
 #  * remove blank lines
 #  * remove trailing whitespace
 
-set -e
+set -o errexit    # Exit immediately if a command exits with a non-zero status
+set -o nounset    # Exit if script tries to use an uninitialized variable
+set -o pipefail   # Produce a failure status if any command in the pipeline fails
 
 if [ $# -ne 0 ]; then
   echo "Do not pass arguments to $0"
--- src/bash/main/clean-java-file.sh
+++ src/bash/main/clean-java-file.sh
@@ -5,7 +5,9 @@
 #  * remove blank lines
 #  * remove trailing whitespace
 
-set -e
+set -o errexit    # Exit immediately if a command exits with a non-zero status
+set -o nounset    # Exit if script tries to use an uninitialized variable
+set -o pipefail   # Produce a failure status if any command in the pipeline fails
 
 if [ $# -ne 1 ]; then
   echo "Usage: $0 MyJavaFile.java"
