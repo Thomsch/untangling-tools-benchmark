caa226626071db6fbcab054b36ea489968e3209f
Michael Ernst <mernst@cs.washington.edu>
2023-08-31 21:21:52 -0700

    Simplify clean_repositories.sh

---
+++ double-blind/clean_repositories.sed
@@ -0,0 +1,28 @@
+s/thomsch/anonymous/
+s/thanhdang2712/anonymous/
+s/thdang/anonymous/
+s/rjust/anonymous/
+s/mernst/anonymous/
+s/tschweiz/anonymous/
+s/thanh/anonymous/
+s/rjust/anonymous/
+s/mernst/anonymous/
+s/Thomas Schweizer/anonymous/
+s/Thomas/anonymous/
+s/Schweizer/anonymous/
+s/Thanh Dang/anonymous/
+s/Thanh/anonymous/
+s/Dang/anonymous/
+s/Rene Just/anonymous/
+s/Just/anonymous/
+s/Rene/anonymous/
+s/Michael Ernst/anonymous/
+s/Mike Ernst/anonymous/
+s/Michael/anonymous/
+s/Mike/anonymous/
+s/Ernst/anonymous/
+s/cs.washington.edu/anonymous/
+s/washington.edu/anonymous/
+s/washington/anonymous/
+s%anonymous/defects4j%rjust/defects4j%
+s/rrjust/rjust/
--- double-blind/clean_repositories.sh
+++ double-blind/clean_repositories.sh
@@ -16,74 +16,29 @@ export out_dir="$1" # The directory where to put cleaned repositories (e.g., 'ar
 mkdir -p "$out_dir"
 
 # Clone repositories
-git clone --depth 1 https://github.com/Thomsch/untangling-tools-benchmark "$out_dir/untangling-tools-benchmark"
-git clone --depth 1 https://github.com/Thomsch/flexeme "$out_dir/flexeme"
+echo "Cloning repositories into $out_dir"
+git -C "$out_dir" clone --depth 1 https://github.com/Thomsch/untangling-tools-benchmark
+git -C "$out_dir" clone --depth 1 https://github.com/Thomsch/flexeme
+echo "Cloning repositories into $out_dir: DONE"
 
-echo "Cloned repositories into $out_dir"
 echo "Cleaning all files in $out_dir"
 
-# Delete .git .github .vscode folders in both repositories
 clean_repo() {
     repo_dir="$1"
     rm -rf "$repo_dir/.git"
     rm -rf "$repo_dir/.github"
     rm -rf "$repo_dir/.vscode"
-
+    find "$out_dir" -name '.DS_Store' -type f -delete
+    find "$out_dir" -type f -print0 | xargs -0 sed -i -f clean_repositories.sed
 }
 
 clean_repo "$out_dir/untangling-tools-benchmark"
 clean_repo "$out_dir/flexeme"
 
-# Replace Github user ids thomsch, thanhdang2712, rjust, mernst with 'anonymous'
-find "$out_dir" -type f -print0 | xargs -0 perl -pi -e 's/thomsch/anonymous/gi'
-find "$out_dir" -type f -print0 | xargs -0 perl -pi -e 's/thanhdang2712/anonymous/gi'
-find "$out_dir" -type f -print0 | xargs -0 perl -pi -e 's/thdang/anonymous/gi'
-find "$out_dir" -type f -print0 | xargs -0 perl -pi -e 's/rjust/anonymous/gi'
-find "$out_dir" -type f -print0 | xargs -0 perl -pi -e 's/mernst/anonymous/gi'
-
-find "$out_dir" -type f -print0 | xargs -0 perl -pi -e 's%anonymous/defects4j%rjust/defects4j%gi' # if we anonymize legitimate link to defects4j, it is suspicious.
-
-# Replace first and or last names with 'Anonymous'
-# Names: Thomas Schweizer, Thanh Dang, Rene Just, Mike Ernst
-# Refactor the code to use a function that takes a full name as a parameter
-
-replace_name() {
-    full_name="$1"
-    first_name=$(echo "$full_name" | cut -d' ' -f1)
-    last_name=$(echo "$full_name" | cut -d' ' -f2)
-
-    find "$out_dir" -type f -print0 | xargs -0 perl -pi -e "s/$first_name/Anonymous/gi"
-    find "$out_dir" -type f -print0 | xargs -0 perl -pi -e "s/$last_name/Anonymous/gi"
-    find "$out_dir" -type f -print0 | xargs -0 perl -pi -e "s/$full_name/Anonymous/gi"
-}
-
-replace_name "Thomas Schweizer"
-replace_name "Thanh Dang"
-replace_name "Rene Just"
-replace_name "Michael Ernst"
-replace_name "Mike Ernst"
-
-
-# Replace school ids tschweiz, thanh, rjust, mernst with 'anonymous'
-find "$out_dir" -type f -print0 | xargs -0 perl -pi -e 's/tschweiz/anonymous/gi'
-find "$out_dir" -type f -print0 | xargs -0 perl -pi -e 's/thanh/anonymous/gi'
-find "$out_dir" -type f -print0 | xargs -0 perl -pi -e 's/rjust/anonymous/gi'
-find "$out_dir" -type f -print0 | xargs -0 perl -pi -e 's/mernst/anonymous/gi'
-
-find "$out_dir" -type f -print0 | xargs -0 perl -pi -e 's%anonymous/defects4j%rjust/defects4j%gi' # if we anonymize legitimate link to defects4j, it is suspicious.
-
-find "$out_dir" -type f -print0 | xargs -0 perl -pi -e 's/rrjust/rjust/gi' # fix places where rjust became rrjust because of the defects4j replacements.
-
-# Replace school name with 'anonymous'
-find "$out_dir" -type f -print0 | xargs -0 perl -pi -e 's/washington/anonymous/gi'
-find "$out_dir" -type f -print0 | xargs -0 perl -pi -e 's/cs.washington/anonymous/gi'
-# The is no occurence of the school acronym anywhere
+echo "Cleaning all files in $out_dir: DONE"
 
 # Move double-blind/README.MD to $OUT_DIR
 cp double-blind/README.MD "$out_dir"
 
 # Remove cleaning script and README.MD from the cleaned repository
 rm -rf "$out_dir/untangling-tools-benchmark/double-blind"
-
-# Delete macOS-specific files
-find "$out_dir" -name '.DS_Store' -type f -delete
