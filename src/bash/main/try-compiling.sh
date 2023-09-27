4af4ba00eb275d99b8e2ae8abe0f4ca6077689f0
Thomas Schweizer <tschweiz@cs.washington.edu>
2023-09-27 14:37:27 -0700

    Cleanup

--- src/bash/main/try-compiling.sh
+++ src/bash/main/try-compiling.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 # Try to compile the projects at a given commit hash from a given file.
 # Arguments:
-# - $1: The file containing the commits.
-# - $2: The output directory where the projects will be cloned and compiled.
+# - $1: Path to the file containing a list of commits to compile.
+# - $2: Directory to store project clones.
 #
 # The results are written to stdout in CSV format with the following columns:
 # - Name of the project
@@ -11,31 +11,33 @@
 # - Time elapsed. In seconds, rounded to the nearest integer.
 #
 # Each version of the project is cloned in a directory named after the project
-# in the output directory. Details error message and execution log is written to
-# 'compile.log' for each project directory.
+# commit hash. The execution log 'compile.log' is written at the root of each
+# clone.
 
 set -o errexit    # Exit immediately if a command exits with a non-zero status
 set -o nounset    # Exit if script tries to use an uninitialized variable
 set -o pipefail   # Produce a failure status if any command in the pipeline fails
 
 if [ $# -ne 2 ] ; then
-    echo 'usage: decompose.sh <commits> <out_dir>'
+    echo 'usage: decompose.sh <commits_file> <clone_dir>'
     exit 1
 fi
 
-export commits="$1" # The file containing the bugs to untangle.
-export out_dir="$2"
+export commits_file="$1"
+export clone_dir="$2"
 
-if ! [ -f "$commits" ]; then
-    echo "$0: file ${commits} not found. Exiting."
+if ! [ -f "$commits_file" ]; then
+    echo "$0: file ${commits_file} not found. Exiting."
     exit 1
 fi
 
-if ! [ -d "$out_dir" ]; then
-    echo "$0: directory ${out_dir} not found. Exiting."
+if ! [ -d "$clone_dir" ]; then
+    echo "$0: directory ${clone_dir} not found. Exiting."
     exit 1
 fi
 
+# Retrieves the default Java version in the format "JAVA<VERSION>". For example,
+# "JAVA8", "JAVA11".
 get_java_version() {
   # Get the Java version and store it in a variable
   java_version=$(java -version 2>&1 | awk -F '"' '/version/ {print $2}')
@@ -54,16 +56,21 @@ get_java_version() {
   echo "$java_format"
 }
 
-untangle_with_tools(){
+# Try to compile a commit and write the compilation result to stdout.
+# Arguments:
+# 1) Project name
+# 2) Project VCS URL
+# 3) Commit hash
+compile(){
   local project_name="$1"
   local vcs_url="$2"
   local commit_hash="$3"
-  local short_commit_fix="${commit_hash:0:6}"
 
-  repository="${out_dir}/projects/${project_name}"
+  local short_commit_fix="${commit_hash:0:6}"
+  local repository="${clone_dir}/${project_name}_${short_commit_fix}"
 
   # TODO: Find a way to speed this up.
-  # IDEA1: Use a local clone of the repository.
+  # IDEA: Use a local clone of the repository and copy clone.
   START="$(date +%s.%N)"
   git clone -q "$vcs_url" "$repository" || exit 1
   cd "$repository" || exit 1
@@ -86,11 +93,10 @@ untangle_with_tools(){
 }
 
 export -f get_java_version
-export -f untangle_with_tools
+export -f compile
 
-# TODO: Try running on multiple versions of Java.
 java_version=$(get_java_version)
 export java_version
 
 printf "%s,%s,%s,%s\n" "project_name" "commit_hash" "compilation_status" "elapsed_time"
-tail -n+2 "$commits" | parallel --colsep "," untangle_with_tools {}
+tail -n+2 "$commits_file" | parallel --colsep "," compile {}
