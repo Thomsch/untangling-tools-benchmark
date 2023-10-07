2d371c44abf29f121dc61276cbc12dfe993c6584
Michael Ernst <mernst@cs.washington.edu>
2023-10-07 11:48:25 -0700

    Code review changes

--- src/bash/main/lltc4j/untangle_lltc4j_commit.sh
+++ src/bash/main/lltc4j/untangle_lltc4j_commit.sh
@@ -7,7 +7,7 @@
 
 # The decomposition results are written to decomposition/smartcommit/<D4J bug>/ and ~/decomposition/<D4J bug>/flexeme/<D4J bug>/ subfolder.
 # - smartcommit/diffs: JSON files storing SmartCommit hunk-based decomposition results
-# - smartcommit/time.csv: Run time allocated for untangling by SmartCommit
+# - smartcommit/time.csv: Run time spent by SmartCommit
 
 set -o nounset    # Exit if script tries to use an uninitialized variable
 set -o pipefail   # Produce a failure status if any command in the pipeline fails
@@ -17,7 +17,7 @@ if [ $# -ne 3 ] ; then
     exit 1
 fi
 
-# Returns the project_name name from an URL.
+# Returns the project name from a URL.
 get_project_name_from_url() {
   local url="$1"
   local filename="${url##*/}"
@@ -30,15 +30,15 @@ commit_hash="$2"
 out_dir="$3"
 
 project_name=$(get_project_name_from_url "$vcs_url")
-short_commit_fix="${commit_hash:0:6}"
+short_commit_hash="${commit_hash:0:6}"
 
-export smartcommit_untangling_results_dir="${out_dir}/decomposition/smartcommit/${project_name}_${short_commit_fix}"
+export smartcommit_untangling_results_dir="${out_dir}/decomposition/smartcommit/${project_name}_${short_commit_hash}"
 export project_clone_dir="${out_dir}/projects/${project_name}"
 
 mkdir -p "$out_dir"
 
 echo ""
-echo "Untangling project_name $vcs_url, revision ${short_commit_fix}"
+echo "Untangling project_name $vcs_url, revision ${short_commit_hash}"
 
 # Clone the repo if it doesn't exist
 if [ ! -d "${project_clone_dir}" ] ; then
@@ -54,16 +54,16 @@ echo ""
 
 # Untangle with SmartCommit
 if [ -d "$smartcommit_untangling_results_dir" ]; then
-  echo 'Untangling with SmartCommit ............................................. CACHED'
+  echo 'Untangling with SmartCommit .......................................... CACHED'
 else
-  echo 'Untangling with SmartCommit ...............................................'
+  echo 'Untangling with SmartCommit ..........................................'
   START_DECOMPOSITION="$(date +%s.%N)"
   "${JAVA11_HOME}/bin/java" -jar lib/smartcommitcore-1.0-all.jar -r "$project_clone_dir" -c "$commit_hash" -o "$smartcommit_untangling_results_dir"
   END_DECOMPOSITION="$(date +%s.%N)"
   DIFF_DECOMPOSITION="$(echo "$END_DECOMPOSITION - $START_DECOMPOSITION" | bc)"
   mkdir -p smartcommit_untangling_results_dir
-  echo "${project_name},${short_commit_fix},smartcommit,${DIFF_DECOMPOSITION}" > "${smartcommit_untangling_results_dir}/time.csv"
-  echo 'Untangling with SmartCommit ............................................... OK'
+  echo "${project_name},${short_commit_hash},smartcommit,${DIFF_DECOMPOSITION}" > "${smartcommit_untangling_results_dir}/time.csv"
+  echo 'Untangling with SmartCommit .......................................... DONE'
 fi
 
 
@@ -71,16 +71,16 @@ fi
 echo ""
 smartcommit_result_out="${smartcommit_untangling_results_dir}/smartcommit.csv"
 if [ -f "$smartcommit_result_out" ] ; then
-  echo 'Parsing SmartCommit results ............................................. CACHED'
+  echo 'Parsing SmartCommit results .......................................... CACHED'
   decompose_exit_code=0
 else
-  echo 'Parsing SmartCommit results ...............................................'
+  echo 'Parsing SmartCommit results ..........................................'
   if python3 src/python/main/smartcommit_results_to_csv.py "${smartcommit_untangling_results_dir}/${project_name}/${commit_hash}" "$smartcommit_result_out"
   then
-      echo 'Parsing SmartCommit results ............................................... OK'
+      echo 'Parsing SmartCommit results .......................................... OK'
       decompose_exit_code=0
   else
-      echo -ne 'Parsing SmartCommit results ............................................. FAIL'
+      echo 'Parsing SmartCommit results .......................................... FAIL'
       decompose_exit_code=1
   fi
 fi
