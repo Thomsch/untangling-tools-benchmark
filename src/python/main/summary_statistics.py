1a268427f7f088fe4facbfbfd1afb12315b4347e
thanhdang2712 <danghuyenthanh2003@gmail.com>
2023-10-17 14:37:46 -0400

    Reformat Black

--- src/python/main/summary_statistics.py
+++ src/python/main/summary_statistics.py
@@ -5,6 +5,7 @@ from unidiff import PatchSet
 from unidiff.constants import LINE_TYPE_CONTEXT
 import diff_metrics
 
+
 def count_all_changed_lines(patch):
     all_changed_lines = 0
     for file in patch:
@@ -17,6 +18,7 @@ def count_all_changed_lines(patch):
                 all_changed_lines += 1
     return all_changed_lines
 
+
 def main():
     """
     Implement the logic of the script. See the module docstring.
@@ -24,7 +26,9 @@ def main():
     args = sys.argv[1:]
 
     if len(args) != 4:
-        print("usage: patch | python3 diff_metrics.py <project> <bug_id> <repo_root> <diff version>")
+        print(
+            "usage: patch | python3 diff_metrics.py <project> <bug_id> <repo_root> <diff version>"
+        )
         sys.exit(1)
 
     project = args[0]
@@ -40,20 +44,22 @@ def main():
         sizes = [0] * len(diff_sets)
         for i in range(len(diff_sets)):
             diff_file = diff_sets[i]
-            patch = PatchSet.from_filename(path.join(repository, "diff", diff_file), encoding="latin-1")
+            patch = PatchSet.from_filename(
+                path.join(repository, "diff", diff_file), encoding="latin-1"
+            )
             sizes[i] = count_all_changed_lines(patch)
-        print(
-            f"{project},{vid},{sizes[0]},{sizes[1]},{sizes[2]}"
-        )
-    else: # Clean version
+        print(f"{project},{vid},{sizes[0]},{sizes[1]},{sizes[2]}")
+    else:  # Clean version
         diff_sets = clean_diffs
         sizes = [0] * len(diff_sets)
         for i in range(len(diff_sets)):
             diff_file = diff_sets[i]
-            patch = PatchSet.from_filename(path.join(repository, "diff", diff_file), encoding="latin-1")
+            patch = PatchSet.from_filename(
+                path.join(repository, "diff", diff_file), encoding="latin-1"
+            )
             sizes[i] = diff_metrics.count_changed_lines(patch)
-        print(
-            f"{project},{vid},{sizes[0]},{sizes[1]},{sizes[2]}"
-        )
+        print(f"{project},{vid},{sizes[0]},{sizes[1]},{sizes[2]}")
+
+
 if __name__ == "__main__":
     main()
