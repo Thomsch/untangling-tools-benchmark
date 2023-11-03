bdc44ba275379e489344aa38e7f9dcf9de9f6aa2
Thomas Schweizer <tschweiz@cs.washington.edu>
2023-11-02 17:39:45 -0700

    Add file-based untangling to refactored pipeline

---
+++ src/bash/main/lltc4j/tool_file.sh
@@ -0,0 +1,52 @@
+# Bash script that implements the tool_file interface for the LLTC4J dataset.
+
+# Check that the environment variables are set for this tool.
+check_environment() {
+  # File-based approach doesn't require any environment variables.
+  return 0
+}
+
+# Check if the untangling tool has already been run on the commit.
+#
+# Arguments:
+# - $1: The directory where the untangling results are stored.
+has_untangling_output() {
+  local untangling_output_dir="$1"
+
+  local untangling_result_file="${untangling_output_dir}/untangling.csv"
+  [ -f "$untangling_result_file" ]
+}
+
+# Untangles a commit from the LLTC4J dataset using $tool_name.
+#
+# Arguments:
+# - $1: The directory containing the repository for the project (ignored by this implementation).
+# - $2: The ground truth file for the commit.
+# - $3: Output directory where the untangling results will be stored.
+untangle_commit() {
+  local ground_truth_file="$2"
+  local untangling_output_dir="$3"
+
+  local untangling_result_file="${untangling_output_dir}/untangling.csv"
+
+  if ! [ -f "$ground_truth_file" ]; then
+    echo "Ground truth file not found: ${ground_truth_file}"
+    return 1
+  fi
+
+  python3 src/python/main/filename_untangling.py "${ground_truth_file}" "${untangling_result_file}"
+  return $?
+}
+
+# Exports the untangling results to a CSV file.
+#
+# Arguments:
+# - $1: The directory where the untangling results are stored.
+# - $2: The CSV file where the untangling results will be exported.
+export_untangling_output() {
+  local untangling_output_dir="$1"
+  local untangling_export_file="$2"
+
+  local untangling_result_file="${untangling_output_dir}/untangling.csv"
+  cp "$untangling_result_file" "$untangling_export_file"
+}
--- src/bash/main/lltc4j/untangle_lltc4j_commits.sh
+++ src/bash/main/lltc4j/untangle_lltc4j_commits.sh
@@ -16,13 +16,23 @@
 # Tool specific arguments are provided via environment variables. Run
 # this script with the tool's name to see the required arguments.
 #
+# The result of each untangling process is output to stdout in the following
+# format: <commit_identifier> <status> (time: <time>) [<log_file>].
+# - <commit_identifier>: The identifier of the commit being untangled.
+# - <status>: The status of the untangling process. Possible values are:
+#   - CACHED: The untangling results were already computed and cached.
+#   - OK: The untangling process succeeded.
+#   - UNTANGLING_FAIL: The untangling process failed.
+#   - EXPORT_FAIL: The export of the untangling results failed.
+#
+# Logging and errors messages are written to stderr.
 
 set -o errexit    # Exit immediately if a command exits with a non-zero status
 set -o nounset    # Exit if script tries to use an uninitialized variable
 set -o pipefail   # Produce a failure status if any command in the pipeline fails
 
 if [ $# -ne 3 ] ; then
-    echo "usage: $0 <commits_file> <results_dir> <tool_name>"
+    echo "usage: $0 <commits_file> <results_dir> <tool_name>" >&2
     exit 1
 fi
 
@@ -31,13 +41,13 @@ export results_dir="$2"
 export tool_name="$3"
 
 if ! [ -f "$commits_file" ]; then
-    echo "$0: commit file ${commits_file} not found. Exiting."
+    echo "$0: commit file ${commits_file} not found. Exiting." >&2
     exit 1
 fi
 
 if ! [ -d "$results_dir" ]; then
-    echo "$0: directory ${results_dir} not found. Exiting."
-    echo "Please generate the ground truth first."
+    echo "$0: directory ${results_dir} not found. Exiting." >&2
+    echo "Please generate the ground truth first." >&2
     exit 1
 fi
 
@@ -49,66 +59,151 @@ ALLOWED_TOOLS=("$FLEXEME_TOOL" "$SMARTCOMMIT_TOOL" "$FILE_TOOL")
 if [[ ! " ${ALLOWED_TOOLS[*]} " == *" ${tool_name} "* ]]; then
     formatted_tools=$(printf "'%s', " "${ALLOWED_TOOLS[@]}")
     formatted_tools="${formatted_tools%, }" # Remove the trailing comma and space.
-    echo "Invalid untangling tool: '$tool_name'."
-    echo "Allowed tools are: $formatted_tools"
+    echo "Invalid untangling tool: '$tool_name'." >&2
+    echo "Allowed tools are: $formatted_tools" >&2
     exit 1
 fi
 
-# Prepare the environment for each tool.
-if [ "$tool_name" == "$FLEXEME_TOOL" ]; then
-
-  # Check for JAVAC Traces environment variable.
-  if [ -z "${JAVAC_TRACES_DIR:-}" ]; then
-    echo "Please set the JAVAC_TRACES_DIR environment variable to the directory containing the Javac traces."
-    echo "See the script try-compiling.sh for more information."
-    exit 1
-  fi
-
-  export PYTHONHASHSEED=0
-  export workdir="${results_dir}/repositories"
-  export logs_dir="${results_dir}/logs"
-
-  mkdir -p "$workdir"
-  mkdir -p "$logs_dir"
-
-elif [ "$tool_name" == "$SMARTCOMMIT_TOOL" ]; then
-
-  java_version="$(java -version 2>&1 | awk -F '"' '/version/ {print $2}' | cut -c1-3)"
-  if [ "$java_version" != "1.8" ] ; then
-    echo "$0: please use Java 8 instead of ${java_version}"
-    exit 1
-  fi
-
-elif [ "$tool_name" == "$FILE_TOOL" ]; then
-
-  export workdir="${results_dir}/repositories"
-  export logs_dir="${results_dir}/logs"
-
-  mkdir -p "$workdir"
-  mkdir -p "$logs_dir"
-
-fi
-
+# Check environment variables and load utility functions for LLTC4J.
 SCRIPT_DIR="$(cd "$(dirname "$0")" && pwd -P)"
 set -o allexport
 . "$SCRIPT_DIR/check-environment-lltc4j.sh"
 . "$SCRIPT_DIR/lltc4j_util.sh"
 set +o allexport
 
-untangle_and_parse_lltc4j() {
-  if [ "$tool_name" == "$FLEXEME_TOOL" ]; then
-    untangle_flexeme "$1" "$2"
-  elif [ "$tool_name" == "$SMARTCOMMIT_TOOL" ]; then
-    untangle_smartcommit "$1" "$2"
-  elif [ "$tool_name" == "$FILE_TOOL" ]; then
-    untangle_file "$1" "$2"
-  else
-    echo "Invalid untangling tool: '$tool_name'."
+# Verify that the script for the tool exists.
+script_for_tool="$SCRIPT_DIR/tool_$tool_name.sh"
+if ! [ -f "$script_for_tool" ]; then
+    echo "Script for tool '$tool_name' not found: '$script_for_tool'." >&2
+    exit 1
+fi
+
+# Verify that the script for the tool defines the required functions for an untangling tool.
+# Required functions:
+# - check_environment: Checks any pre-requisites for the tool to function. The function should exit with an error if the pre-requisites are not met.
+
+# shellcheck source=/dev/null
+. "$script_for_tool"
+
+if ! [[ $(type -t check_environment) == function ]]; then
+  echo "Function 'check_environment' not found in '$script_for_tool'." >&2
+  exit 1
+fi
+check_environment
+
+if ! [[ $(type -t has_untangling_output) == function ]]; then
+  echo "Function 'has_untangling_output' not found in '$script_for_tool'." >&2
+  exit 1
+fi
+export -f has_untangling_output
+
+if ! [[ $(type -t untangle_commit) == function ]]; then
+  echo "Function 'untangle_commit' not found in '$script_for_tool'." >&2
+  exit 1
+fi
+export -f untangle_commit
+
+if ! [[ $(type -t export_untangling_output) == function ]]; then
+  echo "Function 'untangle_commit' not found in '$script_for_tool'." >&2
+  exit 1
+fi
+export -f export_untangling_output
+
+export untangling_tool_output_dir="${results_dir}/decomposition/$tool_name"
+mkdir -p "$untangling_tool_output_dir"
+
+export logs_dir="${results_dir}/logs"
+mkdir -p "$logs_dir"
+
+# Clone missing directories. Needs to be done before running the untangling process otherwise
+# some processes will think the repository has been cloned when it hasn't finished cloning yet.
+clone_repository() {
+  local vcs_url="$1"
+  local project_name
+  project_name="$(get_project_name_from_url "$vcs_url")"
+
+  local project_repository_dir="${results_dir}/repositories/${project_name}"
+
+  # Check if the repo for this project is already cloned.
+  if git clone -q "$vcs_url" "$project_repository_dir" > /dev/null 2>&1; then
+    echo "Cloned $vcs_url in $project_repository_dir because it didn't exist yet." >&2
   fi
+}
+export -f clone_repository
+export repositories_directory="${results_dir}/repositories"
+mkdir -p "$repositories_directory"
+tail -n+2 "$commits_file" | parallel --colsep "," clone_repository {}
+
+
+#mkdir -p "$results_dir" # Create the root directory if it doesn't exists yet.
+
+#export smartcommit_untangling_root_dir="${results_dir}/decomposition/smartcommit"
+#export flexeme_untangling_dir="${results_dir}/decomposition/flexeme"
+#export smartcommit_result_dir="${smartcommit_untangling_root_dir}/${project_name}/${commit_hash}"
+
+# Untangles a commit from the LLTC4J dataset using $tool_name.
+# Arguments:
+# - $1: The URL of the git repository for the project.
+# - $2: The commit hash to untangle.
+untangle_and_parse_lltc4j() {
+  local vcs_url="$1"
+  local commit_hash="$2"
+  local project_name
+  project_name="$(get_project_name_from_url "$vcs_url")"
+  short_commit_hash="${commit_hash:0:6}"
+  commit_identifier="${project_name}_${short_commit_hash}"
+
+  local commit_result_dir="${results_dir}/evaluation/${commit_identifier}"
+  local project_repository_dir="${results_dir}/repositories/${project_name}"
+  local untangling_output_dir="$untangling_tool_output_dir/$commit_identifier"
+
+  mkdir -p "$untangling_output_dir"
+
+  local untangling_export_file="${commit_result_dir}/${tool_name}.csv"
+  local log_file="${logs_dir}/${commit_identifier}_${tool_name}.log"
+  local ground_truth_file="${commit_result_dir}/truth.csv"
+
+  # TODO: Only do that if we need to untangle.
+  # Copy the repository to a temporary directory to enable parallelization.
+  local tmp_repository_dir
+  tmp_repository_dir="$(mktemp -d)"
+  cp -r "$project_repository_dir" "$tmp_repository_dir"
+
+  START="$(date +%s.%N)"
+
+  # Check if the untangling results alreay exist for this commit.
+  # If it does, then the untangling result exists and we can skip the untangling process.
+  # Otherwise, we need to untangle the commit.
+  if [ -f "$untangling_export_file" ]; then
+    status_string="CACHED"
+  elif has_untangling_output "$untangling_output_dir" || untangle_commit "$tmp_repository_dir" "$ground_truth_file" "$untangling_output_dir" > "$log_file" 2>&1; then
+    status_string="UNTANGLING_SUCCESS"
+  else
+    status_string="UNTANGLING_FAIL"
+  fi
+
+  # If the untangling tool produced an output, then export it to the CSV format.
+  if [ "$status_string" == "UNTANGLING_SUCCESS" ]; then
+    if export_untangling_output "$untangling_output_dir" "$untangling_export_file" > "$log_file" 2>&1; then
+      status_string="OK"
+    else
+      status_string="EXPORT_FAIL"
+    fi
+  fi
+
+  END="$(date +%s.%N)"
+  ELAPSED="$(echo "$END - $START" | bc)" # Must use `bc` because the computation is on floating-point numbers.
+  printf "%-20s %-20s (time: %.0fs) [%s]\n" "${commit_identifier}" "${status_string}" "${ELAPSED}" "${log_file}"
 }
 
 export -f untangle_and_parse_lltc4j
 
+START_TIME=$(date +%s)
 tail -n+2 "$commits_file" | parallel --colsep "," untangle_and_parse_lltc4j {}
+END_TIME=$(date +%s)
+ELAPSED_TIME=$((END_TIME - START_TIME))
+ELAPSED_TIME_FORMATTED=$(date -u -d @"${ELAPSED_TIME}" +"%H:%M:%S")
 
-echo "Untangling completed."
+echo "" >&2
+echo "Untangling completed with tool '${tool_name}'." >&2
+echo "Total elapsed time: ${ELAPSED_TIME_FORMATTED}." >&2
