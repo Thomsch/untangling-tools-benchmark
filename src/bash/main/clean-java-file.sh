4f8aba7d401c56f13303b396c7c5c11e646954d2
Thomas Schweizer <tschweiz@cs.washington.edu>
2023-11-14 15:18:20 -0800

    Fix c pre-processor incompatibility between Linux and MacOS

--- src/bash/main/clean-java-file.sh
+++ src/bash/main/clean-java-file.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 
 # This script modifies a Java file in place to:
 #  * remove comments
@@ -7,6 +7,7 @@
 
 set -o errexit    # Exit immediately if a command exits with a non-zero status
 set -o nounset    # Exit if script tries to use an uninitialized variable
+set -o pipefail   # Produce a failure status if any command in the pipeline fails
 
 if [ $# -ne 1 ]; then
   echo "Usage: $0 MyJavaFile.java"
@@ -15,5 +16,16 @@ fi
 
 file="$1"
 
-cpp -fpreprocessed -dD -E "$file" | grep -v '^#' | sed 's/[ \t]*$//' | grep -v '^$' | grep -v '^\s*//' > "$file.cleaned"  # Remove in-line, block comments, trailing whitespaces, and blank lines
+# Remove in-line, block comments, trailing whitespaces, and blank lines
+if [ "$(uname)" = "Darwin" ]; then
+  alias pre_processor='clang -x c++ -E'
+elif [ "$(uname)" = "Linux" ]; then
+  alias pre_processor='cpp -fpreprocessed -dD -E'
+else
+  echo "Unsupported operating system."
+  exit 1
+fi
+
+pre_processor "$file" | grep -v '^#' | sed 's/[ \t]*$//' | grep -v '^$' | grep -v '^\s*//' > "$file.cleaned"
+
 mv -f "$file.cleaned" "$file"
