275bea20b14f70a8b6dca4a4c469c420ec19db79
Thomas Schweizer <tschweiz@cs.washington.edu>
2023-11-16 13:51:40 -0800

    Fix return instead of exit

--- src/bash/main/score_bug.sh
+++ src/bash/main/score_bug.sh
@@ -60,7 +60,7 @@ else
       echo 'Untangling with file-based approach .................................. OK'
   else
       echo -ne 'Untangling with file-based approach .................................. FAIL\r'
-      return 1                # Return exit code 1 to mark this run as FAIl when called in score.sh
+      exit 1                # Return exit code 1 to mark this run as FAIl when called in score.sh
   fi
 fi
 
