50cc3bc64c78b5b55b1bccc0ddd241b4f8fab00d
Thomas Schweizer <tschweiz@cs.washington.edu>
2023-12-13 12:13:32 -0800

    Fix logic

--- src/python/main/newmetrics/count_lines_in_tool_untangling.py
+++ src/python/main/newmetrics/count_lines_in_tool_untangling.py
@@ -16,25 +16,34 @@ from .. import evaluation_results
 FILE_NAME="flexeme.csv"
 COLUMNS = ['project', 'commit_id', 'tool_lines', 'truth_lines']
 
+KEY_COLUMNS = ["file", "source", "target"]
+
+def remove_duplicate_lines(df: pd.DataFrame) -> pd.DataFrame:
+    return df.drop_duplicates(subset=KEY_COLUMNS)
+
 def main(directory:str):
     ground_truth_files = evaluation_results.retrieve_ground_truth_files(directory)
 
     data = []
     for truth_file in ground_truth_files:
         truth_df = evaluation_results.read_ground_truth(truth_file)
+        truth_df = remove_duplicate_lines(truth_df)
 
-        commit_folder = os.path.basename(os.path.dirname(truth_file))
+        commit_folder = os.path.dirname(truth_file)
         project, commit_id = commit_folder.split("_")
 
         flexeme_file = os.path.join(commit_folder, FILE_NAME)
 
         if os.path.exists(flexeme_file):
             flexeme_df = evaluation_results.read_tool_untangling(flexeme_file)
-            flexeme_df = flexeme_df.drop_duplicates(subset=["file", "source", "target"])
+            flexeme_df = remove_duplicate_lines(flexeme_df)
         else:
             flexeme_df = pd.DataFrame(columns=evaluation_results.GROUND_TRUTH_COLUMNS)
 
-        data.append([project, commit_id, len(flexeme_df), len(truth_df)])
+        # Remove lines from flexeme not in the ground truth.
+        merged_df = pd.merge(truth_df, flexeme_df, on=KEY_COLUMNS, how='inner')
+
+        data.append([project, commit_id, len(merged_df), len(truth_df)])
 
     result_df = pd.DataFrame(data, columns=COLUMNS)
     result_df["commit_id"] = result_df["commit_id"].astype(str)
